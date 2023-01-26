48ad261442f3431f06790174bf57cb670a49b65f
Jeandson Barros <geoo677@gmail.com>
2023-01-25 23:43:49 -0300

    first commit

---
+++ src/main/java/com/backend/links/FavoriteLinksApplication.java
@@ -0,0 +1,13 @@
+package com.backend.links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/links/FavoriteLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.links;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
